c17e7550dcbe56fb56b83060faf02313848623a9
malymaul <codewithmal@gmail.com>
2024-03-17 11:33:09 -0400

    Added function getComputerChoice() which returns random choice of rock, paper, scissors

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ let choice = ['rock', 'paper', 'scissors'];
 // Computer choice function
 let getComputerChoice = choiceArr => {
     const randomIndex = Math.floor(Math.random() * choiceArr.length) // Generates a random index
+    return choiceArr[randomIndex];
 };
 
+const cpu = getComputerChoice(choice);
 
-
+console.log(cpu);
