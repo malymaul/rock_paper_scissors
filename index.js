cbe8bec2058d75cc3992ee6611a8b5c7ed652bf1
malymaul <codewithmal@gmail.com>
2024-03-17 11:28:12 -0400

    Created random index variable that returns a random index from the array

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
+// Test
+console.log('Hello, world!')
 
-
-let choices = ['rock', 'paper', 'scissors'];
+let choice = ['rock', 'paper', 'scissors'];
 
 // Computer choice function
-let getComputerChoice = choice => {
-    // Generates a rndom index based on the passed in Arrays length
-     const randomIndex = Math.floor(Math.random() * choice.length);
-    //  Return the item from the array at the random index
-    return choice[randomIndex];
-}
+let getComputerChoice = choiceArr => {
+    const randomIndex = Math.floor(Math.random() * choiceArr.length) // Generates a random index
+};
+
+
 
